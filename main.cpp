210f5d9fd4f1b8894927d7a3b480dad2d55cd4c4
Creature547 <greminanya@yandex.ru>
2025-05-18 21:33:22 +0300

    ne

--- main.cpp
+++ main.cpp
@@ -5,14 +5,19 @@ class Person {
 //  public:
     // const char* name;
     char name[20];
-    int age;
+    int age = 0;
+    inline static int counter = 0;
 public:
     void print() { std::cout << "1 Name: " << name << "\tAge: " << age << std::endl; }
     void print(int) { std::cout << "2 Name: " << name << "\tAge: " << age << std::endl; }
     Person()
         : name("Name")
         , age(12)
-    {std::cout << "Person(1) Default ctr" << std::endl;}
+    {
+        std::cout << "Person(1) Default ctr" << std::endl;
+        counter++;
+    }
+
     Person(const char* new_name, int new_age = 9) {
         std::cout << "Person(2)" << std::endl;
         // name = new_name;
@@ -21,6 +26,7 @@ public:
         }
         age = new_age;
         std::cout << "Person has been created" << std::endl;
+        counter++;
     }
 
     friend std::ostream& operator << (std::ostream &out, const Person& p) {
@@ -40,22 +46,55 @@ public:
         input >> p.age;
         return input;
     }
-    int homework(age);
+    int homework(int);
+    int homework(int) const;
+    int homework(double) ;
+
+    static int total() {
+        // std::cout << age << std::endl;
+        return counter;
+    }
 };
 
-int Person::homework(age) {
-    std::cout << age << std::endl;
-    return age;
+// int Person::counter = 0;
+
+
+
+int Person::homework(int newage) {
+    std::cout << "homework(int newage) "<< age << std::endl;
+    age = newage;
+    return this->age;
+}
+
+int Person::homework(int newage) const{
+    std::cout << "homework(int newage) const "<< age << std::endl;
+    return this->age;
+}
+
+int Person::homework(double newage) {
+    std::cout << "homework(double newage) "<< age << std::endl;
+    return this->age;
 }
 
 int main() {
     Person tom("Tom", 38);  // создаем объект - вызываем конструктор
     tom.print(12);
-    tom.homework(12);
+    int a = tom.homework(190);
+    std::cout << "counter: " << Person::total() << std::endl;
+    std::cout << a << std::endl;
+    const Person Ben("ben", 20);
+    std::cout << "counter: " << Person::total() << std::endl;
+    std::cout << Ben.homework(10) << std::endl;
+    std::cout << tom.homework(78.3) << std::endl;
+    std::cout << Ben.homework(50.2) << std::endl;
     Person ann;  // создаем объект - вызываем конструктор
+    std::cout << "counter: " << Person::total() << std::endl;
     ann.print();
     // std::cin >> tom;
     // std::cin >> ann;
     // std::cout << &tom << " " << tom << std::endl;
     // std::cout << &ann << " " << ann << std::endl;
+    // Person::print();
+    std::cout << ann.total() << std::endl;
+    std::cout << Person::total() << std::endl;
 }
