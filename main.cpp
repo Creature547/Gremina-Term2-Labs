e4119b782067b50943248acd709e46a939212a6b
Creature547 <greminanya@yandex.ru>
2025-05-28 19:12:24 +0300

    new

--- main.cpp
+++ main.cpp
@@ -20,16 +20,36 @@ int main() {
         std::cin.ignore();
 
         switch (choice) {
-            case 1: task1(); break;
-            case 2: task2(); break;
-            case 3: task3(); break;
-            case 4: task4(); break;
-            case 5: task5(); break;
-            case 6: task6(); break;
-            case 7: task7(); break;
-            case 8: task8(); break;
-            case 9: task9(); break;
-            default: std::cout << "Неверный номер задания.\n"; break;
+            case 1:
+                task1();
+                break;
+            case 2:
+                task2();
+                break;
+            case 3:
+                task3();
+                break;
+            case 4:
+                task4();
+                break;
+            case 5:
+                task5();
+                break;
+            case 6:
+                task6();
+                break;
+            case 7:
+                task7();
+                break;
+            case 8:
+                task8();
+                break;
+            case 9:
+                task9();
+                break;
+            default:
+                std::cout << "Неверный номер задания.\n";
+                break;
         }
 
         std::cout << "\nХотите продолжить выполнение программы? (1 — да / 0 — нет): ";
